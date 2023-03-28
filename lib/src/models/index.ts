e205c33d93f2a4fa2bb64d2c8423865bdf315af2
Artem Polikarpov <artemiypolikarpov@gmail.com>
2023-03-28 16:12:42 +0300

    [lib] Add pre-commit lint:fix hook

--- lib/src/models/index.ts
+++ lib/src/models/index.ts
@@ -1,3 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 /** Any-function that accepts any arguments and returns any value. **/
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type TAnyFunction = (...args: any[]) => any
