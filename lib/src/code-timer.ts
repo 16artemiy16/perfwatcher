52b611db7f3cfea8706d6ad56d7043d64ca2eeff
Artem Polikarpov <artemiypolikarpov@gmail.com>
2023-03-30 08:25:40 +0300

    [lib] update husky to lint only staged files

--- lib/src/code-timer.ts
+++ lib/src/code-timer.ts
@@ -1,5 +1,6 @@
 import {TAnyFunction} from './models';
 
+/** Measures the given function execution time. **/
 export const measure = (fn: TAnyFunction) => {
   const start = performance.now();
   fn();
